3e034badbcfee261d37c756b1aa63ff331d8be30
Trần Thị Lài <lai.tran11@hcmut.edu.vn>
2024-05-19 14:43:16 +0700

    update register

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -28,3 +28,28 @@ export const fetchlogin = async(loginData: LoginModel) : Promise<Member[] | null
         return null;
     };
 }
+export interface RegisterModel {
+    fullName: string;
+    Usr: string;
+    Pwd: string;
+}
+export const fetchregister = async(registerData: RegisterModel) : Promise<Member[] | null> =>{
+    try{
+        const response = await fetch(API + 'auth/register',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify(registerData)
+        });
+        if(!response.ok)
+            {
+                throw new Error('Failed to fetch data');
+            }
+        const data: Member[] = await response.json();
+        return data;
+    }catch(error){
+        console.error('Error fetching data', error);
+        return null;
+    };
+}
